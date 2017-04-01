1292950ea5665fe7a7c204693f4491e5e4a956bd
Aidan Tan <league@WorthyMini-6.attlocal.net>
2017-04-01 12:32:54 -0700

    leftoverSpaces

--- src/StringFormatter.java
+++ src/StringFormatter.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringFormatter
 {
-
+	public static void main(String[] args) {
+		List<String> wordList = new ArrayList<String>();
+wordList.add("A");
+wordList.add("frog");
+wordList.add("is");
+	 System.out.println(totalLetters(wordList));
+	}
+	
+public static int totalLetters (List<String> words)
+{
+	int letters=0;
+	for(int i=0; i<words.size(); i++ )
+	{
+	letters+=words.get(i).length();
+	}
+	return letters;
+	
+}
 }
